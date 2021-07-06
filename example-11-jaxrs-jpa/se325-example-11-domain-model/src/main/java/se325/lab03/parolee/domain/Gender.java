9cb792b281d6fc2101e693643666ab231c189c07
Andrew Meads <andrew.meads@auckland.ac.nz>
2021-07-06 16:45:35 +1200

    Deleted wrong stuff

--- example-11-jaxrs-jpa/se325-example-11-domain-model/src/main/java/se325/lab03/parolee/domain/Gender.java
+++
@@ -1,8 +0,0 @@
-package se325.lab03.parolee.domain;
-
-/**
- * Enumerated type to represent gender.
- */
-public enum Gender {
-    MALE, FEMALE, OTHER;
-}
--- example-11-jaxrs-jpa/se325-example-11-domain-model/src/main/java/se325/lab03/parolee/domain/Offence.java
+++
@@ -1,5 +0,0 @@
-package se325.lab03.parolee.domain;
-
-public enum Offence {
-    THEFT, POSSESION_OF_OFFENSIVE_WEAPON, MURDER, RAPE, TAX_EVASION;
-}
